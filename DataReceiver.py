f9c15d76fd31a0caf1ca17ac8f96b5962a423c11
itocska <timarito@gmail.com>
2024-04-24 20:21:15 +0200

    loadConfigurationToDataReceiver() finsihed

--- DataReceiver.py
+++ DataReceiver.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv, dotenv_values
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pyautogui
 
 load_dotenv()
 
@@ -30,4 +31,24 @@ def dataReceiverLogin(driver) :
         print("Velcome header not found")
 
 def loadConfigurationToDataReceiver(driver) :
-    driver.find_element(By.XPATH, "//input[contains(@class,'mat-input-element')]").send_keys("C:\projects\navu_aut\downloads\extracted\konf_TestNetwork_adatfogado_2_2024-04-22_1516.zip")
+    #driver.find_element(By.XPATH, "//input[contains(@class,'mat-input-element')]").send_keys("C:\projects\navu_aut\downloads\extracted\konf_TestNetwork_adatfogado_2_2024-04-22_1516.zip")
+        driver.find_element(By.XPATH, "(//mat-card//mat-icon)[1]").click()
+        time.sleep(1)
+        pyautogui.write(os.getenv("DLOAD_PATH_BSLASH") + "\extracted\konf_TestNetwork_adatfogado_2_2024-04-22_1516.zip")
+        pyautogui.press("enter")
+        try:
+            WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//h2[contains(text(),'Konfigurációs fájl betöltése')]")) )
+        except NoSuchElementException:
+            print("Error in page loading")
+        time.sleep(1)
+        driver.find_element(By.XPATH, "//button/span[contains(text(),'Mentés')]").click()
+        try:
+            WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Információ')]")) )
+        except NoSuchElementException:
+            print("Error in page loading")
+        driver.find_element(By.XPATH, "//span[text()=' Rendben ']").click()
+        time.sleep(0.5)
+        try:
+            WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//h1[text()='Konfiguráció kezelése']")) )
+        except NoSuchElementException:
+            print("Error in page loading")
